3e0dd1ece7f2154d8b621aa9b88599fef5a79bd8
Prince Samuel <prince.samuel@gmail.com>
2012-06-28 23:59:48 -0500

    New DB access layer class.
    * Change SQLite DB to version 3.
    * Added DAL sub-folder to Lib project
    * Added DataManager class under DAL to manage DB access
    * Tested DataManager by opening the connection and running a query.
    Works.

---
+++ DAL/DataManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+
+namespace OpenHardwareMonitor.DAL
+{
+    public class DataManager
+    {
+        private string _dbFile;
+        private SQLiteConnection _sqliteConnection;
+        public DataManager()
+        {
+            _dbFile = "sqlite.s3db";
+            string currentPath;
+            string absolutePath;
+            string connectionString;
+
+            currentPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath); 
+            absolutePath = System.IO.Path.Combine(currentPath, _dbFile);
+
+            connectionString = string.Format(@"Data Source={0}", absolutePath);
+
+            _sqliteConnection = new SQLiteConnection(connectionString);
+        }
+    }
+}
